376ff70f3c660e1c83a3fdb19f8ac662fca3cf74
JaroCiganek <jaroslav.ciganek@student.ukf.sk>
2024-12-20 21:13:12 +0100

    Hodnoty pre tabulku Role
    Insert pre ROLE.

---
+++ database/data/role_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `role` (`idrole`, `code`, `description`, `created_on`, `updated_on`, `valid_from`, `valid_to`) VALUES
+(1, 'STDNT', 'Student', '2024-12-20 20:06:27', NULL, '1900-01-01', '2100-12-31'),
+(2, 'ADMIN', 'Administrator', '2024-12-20 20:06:27', NULL, '1900-01-01', '2100-12-31'),
+(3, 'REVIW', 'Reviewer', '2024-12-20 20:06:27', NULL, '1900-01-01', '2100-12-31');
