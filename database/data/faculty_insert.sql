bbc706fc1ce6347bc76a521c66dbd3dc83534fff
JaroCiganek <jaroslav.ciganek@student.ukf.sk>
2024-12-20 21:04:46 +0100

    Struktura university, faculty, department
    Priklady 2 univerzit, par fakult a vela katedier.

---
+++ database/data/faculty_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO `faculty` (`idfaculty`, `code`, `name`, `university_iduniversity`, `valid_from`, `valid_to`, `created_on`, `updated_on`) VALUES
+(1, 'FPVaI', 'Fakulta prírodných vied a informatiky', 1, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(2, 'FSVaZ', 'Fakulta sociálnych vied a zdravotníctva', 1, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(3, 'FF', 'Filozofická fakulta', 1, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(4, 'FSŠ', 'Fakulta stredoeurópskych štúdií', 1, '2003-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(5, 'PF', 'Pedagogická fakulta', 1, '1963-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(6, 'FF', 'Filozofická fakulta', 2, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(7, 'FMK', 'Fakulta masmediálnej komunikácie', 2, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(8, 'FPV', 'Fakulta prírodných vied', 2, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(9, 'FSV', 'Fakulta sociálnych vied', 2, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL),
+(10, 'FZV', 'Fakulta zdravotníckych vied', 2, '1993-01-01', '2199-12-31', '2024-12-17 20:11:18', NULL);
