2bdcc8f777fef07205730ea3a7e76ab83061a6de
NoroMojsej <luzak.mato33@gmail.com>
2025-01-10 18:29:18 +0100

    Review status DB data

---
+++ database/data/review_status_insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO `review_status` (`idreview_status`, `status`, `status_desc`, `valid_from`, `valid_to`, `created_on`, `updated_on`) VALUES ('1', 'YY', 'Práca bola Schválená.', NULL, NULL, NULL, NULL), ('2', 'NN', 'Práce nebola Schválená.', NULL, NULL, NULL, NULL)
