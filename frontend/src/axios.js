0fe9b9313ef403af9c19a94679a711e8f044c449
PetraMichalcova <117039065+PetraMichalcova@users.noreply.github.com>
2025-01-15 19:11:12 +0100

    Uprava autorizacia

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -8,13 +8,32 @@ const axiosInstance = axios.create({
   },
 });
 
-// Add an interceptor for automatically attaching the token
-axiosInstance.interceptors.request.use((config) => {
+axiosInstance.interceptors.request.use((config) => { // interceptor pre automaticke zachytenie tokenu
   const token = localStorage.getItem('auth_token');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
+});
+
+axiosInstance.interceptors.response.use(response => {
+  return response;
+}, error => {
+  // session expired or is invalid
+  if (error.response && error.response.status === 401) {
+      const message = error.response.data?.message;
+
+      if (message === 'Unauthenticated.') {
+        console.log('Session expired got 401.');
+        alert("Boli ste odhlásený, prihláste sa znova");
+      }
+      // always delete in case of 401
+      localStorage.removeItem('auth_token');
+      localStorage.removeItem('session');
+
+  } 
+  // Return this otherwise error messages will not show in some forms
+  return Promise.reject(error);
 });
 
 export default axiosInstance;
